4238d9b158e9164b8954d76fe8b6040d8a32d2cb
dsolutionsredes-dotcom <dsolutions.redes@gmail.com>
2026-07-26 23:25:19 +0200

    Create Spanish dictionary for multimedia library
    Added Spanish translations for multimedia library terms.

---
+++ src/i18n/dict/es/library.ts
@@ -0,0 +1,9 @@
+import en from '../en/library';
+
+export default {
+  ...en,
+  '素材库': 'Biblioteca multimedia', '上传素材': 'Subir recursos', '导入': 'Importar', '图片': 'Imágenes',
+  '视频': 'Vídeos', '音频': 'Audio', '搜索素材': 'Buscar recursos', '拖入文件或点击上传': 'Arrastra archivos o haz clic para subirlos',
+  '删除素材': 'Eliminar recurso', '下载': 'Descargar', '收藏': 'Favorito', '最近使用': 'Usados recientemente',
+  '全部': 'Todos', '筛选': 'Filtrar', '排序': 'Ordenar',
+} as Record<string, string>;
