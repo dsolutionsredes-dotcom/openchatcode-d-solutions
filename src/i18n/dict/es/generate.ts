9f0e3b10aacbfcffa8bae2c47e366561f7013ed5
dsolutionsredes-dotcom <dsolutions.redes@gmail.com>
2026-07-26 23:22:29 +0200

    Add Spanish translations to generate.ts

---
+++ src/i18n/dict/es/generate.ts
@@ -0,0 +1,3 @@
+import en from '../en/generate';
+
+export default { ...en, '生成': 'Generar', '生成中…': 'Generando…', '重新生成': 'Generar de nuevo', '请输入提示词': 'Escribe una indicación' } as Record<string, string>;
