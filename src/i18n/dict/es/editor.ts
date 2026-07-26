9a884b24aa9997e0a82a6871610814228705ca35
dsolutionsredes-dotcom <dsolutions.redes@gmail.com>
2026-07-26 23:22:15 +0200

    Add Spanish translations for editor messages

---
+++ src/i18n/dict/es/editor.ts
@@ -0,0 +1,3 @@
+import en from '../en/editor';
+
+export default { ...en, '加载编辑器…': 'Cargando editor…', '加载工程…': 'Cargando proyecto…', '工程不存在，返回…': 'El proyecto no existe; volviendo…' } as Record<string, string>;
